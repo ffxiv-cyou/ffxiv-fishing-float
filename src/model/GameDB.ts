add77178f5141282515108ac9fc5512d9f0b414a
Jim Jiang <jim@lotlab.org>
2026-01-22 23:51:09 +0800

    misc: reactive 化 gamedatabase

--- src/model/GameDB.ts
+++ src/model/GameDB.ts
@@ -1,9 +1,18 @@
+import { createSubscriber } from "svelte/reactivity";
+
 export class GameDatabase {
   placeNames: { [key: number]: string } = {};
   itemNames: { [key: number]: string } = {};
   opcodes: { [key: string]: number } = {};
-  
-  constructor() {}
+
+  update: (() => void) | null = null;
+  #subscribe;
+
+  constructor() {
+    this.#subscribe = createSubscriber((update) => {
+      this.update = update;
+    });
+  }
 
   async load(version: string): Promise<void> {
     let placnames = await fetch(`/data/${version}/placename.json`);
@@ -14,22 +23,29 @@ export class GameDatabase {
 
     let opcodes = await fetch(`/data/${version}/opcode.json`);
     this.opcodes = await opcodes.json();
+
+    if (this.update) {
+      this.update();
+    }
   }
 
-  async getVersions(): Promise<{[key: string]: string}> {
+  async getVersions(): Promise<{ [key: string]: string }> {
     let versionsResp = await fetch(`/data/version.json`);
     return await versionsResp.json();
   }
 
   getZoneName(zoneId: number): string {
+    this.#subscribe();
     return this.placeNames[zoneId] || "未知区域(" + zoneId + ")";
   }
 
   getItemName(itemId: number): string {
+    this.#subscribe();
     return this.itemNames[itemId] || "未知物品(" + itemId + ")";
   }
 
   getOpcodes(): { [key: string]: number } {
+    this.#subscribe();
     return this.opcodes;
   }
 }
