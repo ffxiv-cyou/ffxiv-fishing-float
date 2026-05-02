8938ff6e7ca2846bb6888d353a53104433a159d0
Jim Kirisame <jim@lotlab.org>
2026-05-02 20:09:28 +0800

    fix: sentry

--- src/sentry.ts
+++ src/sentry.ts
@@ -3,5 +3,5 @@ import * as Sentry from "@sentry/svelte";
 Sentry.init({
     dsn: "https://8e85b591a0384248a6628949940a351e@sentry.ffxiv.cyou/6",
     sendDefaultPii: true,
-    enabled: import.meta.env.PROD,
+    // enabled: import.meta.env.PROD,
 });
